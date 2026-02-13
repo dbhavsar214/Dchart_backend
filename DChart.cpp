0b7b67e44df80a12af9d1b5b8860146c87b79f25
dbhavsar214 <devbhavsar214@gmail.com>
2026-02-13 06:50:31 -0400

    Dchart Backend Application Setup

---
+++ DChart.cpp
@@ -0,0 +1,13 @@
+// GraphBackend.cpp : Defines the entry point for the application.
+//
+
+#include "Dchart.h"
+
+using namespace std;
+
+int main()
+{	
+	cout << "Dchart Running";
+	//startPriceGenerator();
+	return 0;
+}
