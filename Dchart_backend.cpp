ab218412b81eddd50df9651ad2d5a0eb5e7ca19e
dbhavsar214 <devbhavsar214@gmail.com>
2026-02-16 21:21:38 -0400

    DChart Price generator service setup completed external dependencies

---
+++ Dchart_backend.cpp
@@ -0,0 +1,13 @@
+﻿// Dchart_backend.cpp : Defines the entry point for the application.
+//
+
+#include "Dchart_backend.h"
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Dchart." << endl;
+	return 0;
+}
